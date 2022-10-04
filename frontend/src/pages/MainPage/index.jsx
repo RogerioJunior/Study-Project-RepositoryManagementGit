d6b2294a354796ea663c902048dc2b62e0555345
RogerioJunor <rogeriopcjr@gmail.com>
2022-10-04 18:37:02 -0300

    code main page

--- frontend/src/pages/MainPage/index.jsx
+++ frontend/src/pages/MainPage/index.jsx
@@ -1,7 +1,63 @@
 import React from "react";
 
-const MainPage = () => (
-    <h1>Main Page</h1>
-)
+import "./styles.css";
+
+const MainPage = () => {
+  const handleLogout = () => {
+    console.log("logout");
+  };
+
+  const handleSearch = (query) => {
+    console.log("query", query);
+  };
+
+  const handleClear = () => {
+    console.log("clear");
+  };
+
+  const handleDeleteRepo = () => {
+    console.log("delete repo");
+  }
+
+  const handleAddRepo = () => {
+    console.log("add repo")
+  }
+
+  return (
+    <div id="main">
+      <div className="nav">
+        <h1 className="logo">SisRepo</h1>
+        <button onClick={handleLogout}>Sair</button>
+      </div>
+
+      <div className="search">
+        <label htmlFor="query">Procurar</label>
+        <input type="search" name="query" id="query" />
+        <button onClick={handleClear}>Limpar</button>
+        <button onClick={handleSearch}>Procurar</button>
+      </div>
+
+      <div className="repositories">
+        <h2 className="title">Repositorios</h2>
+
+        <ul className="list">
+          <li className="item">
+            <div className="info">
+              <div className="owner">Facebook</div>
+              <div className="name">react</div>
+            </div>
+            <button onClick={handleDeleteRepo}>X</button>
+          </li>
+        </ul>
+
+        <div className="new">
+            <label htmlFor="new-repo">Novo Repositorio:</label>
+            <input type="url" name="new-repo" id="new-repo" />
+            <button onClick={handleAddRepo}>Adicionar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;
