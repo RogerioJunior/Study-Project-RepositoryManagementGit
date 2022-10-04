b81c6afc43af870c74c3c62272bc88911646909c
RogerioJunor <rogeriopcjr@gmail.com>
2022-10-04 16:19:50 -0300

    create middlewares for auth token user

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,9 +1,23 @@
-export default async (req, res, next) => {
-    const authenticated = false;
+import jwt from "jsonwebtoken";
+import promisify from "util";
 
-  if (authenticated) {
+import authConfig from "../config/auth";
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token was not provided." });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const decoded = await promisify(jwt.verify(token, authConfig.secret));
+    req.userId = decoded.id;
+
     return next();
-  }else {
-    return res.status(401).json();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token." });
   }
 };
